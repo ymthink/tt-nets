ece1a18dbecd9ebe7a988320bfa26465b31069a4
ymthink <yinmiaothink@gmail.com>
2019-04-27 12:22:55 +0800

    commit

---
+++ neuraltalk/train_base.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# 
+# Author: YIN MIAO
+# Time: 2019/4/27 12:22
+
+
+from NeuralTalk import NeuralTalk
+
+
+if __name__ == '__main__':
+    nt = NeuralTalk(is_TT=False)
+    nt.train()
+    # nt.eval()
---
+++ neuraltalk/train_tt.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# 
+# Author: YIN MIAO
+# Time: 2019/4/27 11:41
+
+from NeuralTalk import NeuralTalk
+
+
+if __name__ == '__main__':
+    nt = NeuralTalk(is_TT=True)
+    nt.train()
+    # nt.eval()
+
